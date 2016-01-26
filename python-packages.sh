bc72e4458a2cbe4c9721274359d1f3469f182b7b
dwminer <daltonminer@gmail.com>
2016-01-26 12:12:53 -0500

    Update to distinguish between Python 2 and 3

--- python-packages.sh
+++ python-packages.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+echo "Python 2"
 echo "Pip"
-pip list
+pip2 list
 echo
 echo "Pacman"
-pacman -Q | grep -E "python(2?)-" | sed -r "s/python(2?)-(.*) (([0-9]+.?)+)/\2 \(\3\)/"
+pacman -Q | grep -E "python2-" | sed -r "s/python2-(.*) (([0-9]+.?)+)/\1 \(\2\)/"
+
+echo
+echo "Python 3"
+echo "Pip"
+pip3 list
+echo
+echo "Pacman"
+pacman -Q | grep -E "python-" | sed -r "s/python-(.*) (([0-9]+.?)+)/\1 \(\2\)/"
